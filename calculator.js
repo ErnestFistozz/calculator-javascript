8f488a5ee0f158e7f89a6ad361efaca92950426d
Fistozz <mamba.fistozz@gmail.com>
2023-05-04 14:55:26 +0200

    initial commit

---
+++ calculator.js
@@ -0,0 +1,33 @@
+class Calculator {
+    constructor(x, y) {
+      if(isNaN(x) || isNaN(y)){
+          throw new Error('Cannot initialise by Undefined values')
+      }
+      this.value1 = x;
+      this.value2 = y;
+    }
+    addition = () => {
+      return this.value1 + this.value2;
+    };
+    subtraction = () => {
+      return this.value1 - this.value2;
+    };
+    multiplication = () => {
+      return this.value1 * this.value2;
+    };
+    division = () => {
+      if (this.value2 == 0) {
+        throw new Error("Cannot divide by zero");
+      }
+  
+      return this.value1 / this.value2;
+    };
+    modulus = () => {
+      if (this.value2 == 0) {
+        throw new Error("Cannot modularise by zero");
+      }
+    };
+  }
+  
+  module.exports = { Calculator };
+  
