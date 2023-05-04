f7efb952cd01d15626f5518ec48edf2985c56939
Fistozz <mamba.fistozz@gmail.com>
2023-05-04 15:24:04 +0200

    added subtraction test

--- calculator.test.js
+++ calculator.test.js
@@ -8,5 +8,12 @@ describe("Calculator tests", () => {
         // assert
         expect(result).toBe(3);
       });
-
+      test('subtracting 2 from 5 to give 3', () => {
+        // arrange and act
+        let inst = new cal.Calculator(5,2);
+        let result = inst.subtraction()
+        // assert
+        expect(result).toBe(3);
+      });
+    
 })
